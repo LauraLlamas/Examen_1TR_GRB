efd44450fca136b8d1000131030e0e3a649e76b3
Laura <laurallamasruiz@gmail.com>
2020-12-09 17:52:37 +0100

    Instanciar nave

---
+++ Laura Llamas/Assets/Scripts/MoverNave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverNave : MonoBehaviour
+{
+
+    //Unir el prefab con el instanciador
+    [SerializeField] GameObject Nave;
+    Vector3 pos1;
+    
+    
+    void Start()
+    {
+        pos1 = new Vector3(0, 0, 0);
+        Instantiate(Nave, pos1, Quaternion.identity);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
